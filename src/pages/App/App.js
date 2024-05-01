1d4cc9caefbf7d46980e93f39c5c1c234837de4f
geewjh <geewjh@gmail.com>
2024-05-02 01:28:09 +0800

    fix: add routes for signup and login

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -23,7 +23,10 @@ function App() {
           </Routes>
         </>
       ) : (
-        <AuthPage setUser={setUser} />
+        <Routes>
+          <Route path="/signup" element={<AuthPage setUser={setUser} />} />
+          <Route path="/login" element={<AuthPage setUser={setUser} />} />
+        </Routes>
       )}
     </main>
   );
