345aa4d28016ce2b7ba0a37491c91911e1f19a73
Shermaine <shermaine.lsh@gmail.com>
2024-05-06 02:02:02 +0800

    created sharedexpense-service

--- src/utilities/sharedexpense-api.js
+++ src/utilities/sharedexpense-api.js
@@ -2,8 +2,8 @@ import sendRequest from "./send-request";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-const BASE_URL = `${apiUrl}/api/sharedexpenses`;
-// const BASE_URL = "/api/sharedexpenses";
+const BASE_URL = `${apiUrl}/api/sharedexpense`;
+// const BASE_URL = "/api/sharedexpense";
 
 export function createSharedExpense(sharedExpenseDetails) {
   return sendRequest(`${BASE_URL}/create`, "POST", sharedExpenseDetails);
