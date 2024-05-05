0cbaafc8a1334489baaee36e1ab786dc0d68eab8
Shermaine <shermaine.lsh@gmail.com>
2024-05-06 04:16:14 +0800

    updated correct route

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h3>Home</h3>
+      <div className="statistics"></div>
+    </div>
+  );
 }
--- src/utilities/sharedexpense-api.js
+++ src/utilities/sharedexpense-api.js
@@ -2,8 +2,8 @@ import sendRequest from "./send-request";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-const BASE_URL = `${apiUrl}/api/sharedexpense`;
-// const BASE_URL = "/api/sharedexpense";
+const BASE_URL = `${apiUrl}/api/shared-expenses`;
+// const BASE_URL = "/api/shared-expenses";
 
 export function createSharedExpense(sharedExpenseDetails) {
   return sendRequest(`${BASE_URL}/create`, "POST", sharedExpenseDetails);
