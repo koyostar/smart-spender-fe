ef7d5833299a2600ff871bdb893744531e3ee582
Shermaine <shermaine.lsh@gmail.com>
2024-05-08 00:35:48 +0800

    fix: calculations error

--- src/components/Charts/SummaryChart.jsx
+++ src/components/Charts/SummaryChart.jsx
@@ -2,41 +2,108 @@ import { BarChart } from "@mui/x-charts/BarChart";
 import { AxisConfig } from "@mui/x-charts";
 import { useEffect, useState } from "react";
 import { fetchStatsService } from "../../utilities/statistics-service";
+import { findUsersByID } from "../../utilities/users-api";
 
 export default function SummaryChart() {
+  const [statsLoading, setStatsLoading] = useState(false);
   const [stats, setStats] = useState(null);
+  const [error, setError] = useState(null);
+  const [userDetails, setUserDetails] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
+      setStatsLoading(true);
       try {
         const statsData = await fetchStatsService();
         setStats(statsData);
+        setError(null);
       } catch (error) {
-        console.error("Error fetching statistics:", error);
+        setError(error.message);
+      } finally {
+        setStatsLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  const totalExpenses = stats ? stats.totalExpenses.totalExpenses : null;
+  useEffect(() => {
+    const fetchUserDetailsForStats = async () => {
+      if (
+        stats &&
+        stats.sharedExpenseOwed &&
+        stats.sharedExpenseOwed.usersThatOwes &&
+        stats.sharedExpenseOwed.usersThatOwes.length > 0
+      ) {
+        const userIds = stats.sharedExpenseOwed.usersThatOwes.map(
+          (item) => item.user
+        );
+        console.log("User IDs:", userIds); // Log user IDs
+
+        const userDetailsMap = {};
+        for (const userId of userIds) {
+          try {
+            const userDetails = await findUsersByID(userId);
+            console.log("User details for", userId, ":", userDetails); // Log user details
+
+            userDetailsMap[userId] = userDetails.user.username;
+          } catch (error) {
+            console.error("Error fetching user details:", error);
+            userDetailsMap[userId] = null;
+          }
+        }
+        console.log("User details map:", userDetailsMap); // Log user details map
+
+        setUserDetails(userDetailsMap);
+      }
+    };
+
+    fetchUserDetailsForStats();
+  }, [stats]);
+
+  const expensesCreated = stats ? stats.expensesCreated.expensesCreated : null;
+
+  const totalSharedExpenses = stats
+    ? stats.totalSharedExpenses.totalOwed
+    : null;
   const totalUnpaidSharedExpenses = stats
     ? stats.unpaidSharedExpenses.totalUnpaid
     : null;
+  const totalPaidSharedExpenses = stats
+    ? stats.paidSharedExpenses.totalPaid
+    : null;
   const totalAmountToCollect = stats
-    ? stats.sharedExpenseStats.totalAmountIsOwed
+    ? stats.sharedExpenseOwed && stats.sharedExpenseOwed.totalAmountIsOwed
     : null;
 
+  const totalAmountReceived = stats
+    ? stats.sharedExpensePaid && stats.sharedExpensePaid.totalAmountIsPaid
+    : null;
+
+  const totalExpenses = stats ? stats.totalExpenses : null;
+
+  const UserOwesList = ({ usersThatOwes, userDetails }) => (
+    <div>
+      <ul>
+        {usersThatOwes.map((item, index) => (
+          <li key={index}>
+            {userDetails[item.user]} owed you {item.amountOwed}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
   const summaryStatsData = [
-    totalExpenses,
     totalUnpaidSharedExpenses,
     totalAmountToCollect,
+    totalExpenses,
   ];
 
   const summaryStatsLabel = [
-    "Total Expenses",
     "Unpaid Shared Expenses",
     "Total Amount To Collect",
+    "Total Expenses",
   ];
 
   const valueFormatter = (value) => `$${value}`;
--- src/components/Statistics/UserSummary.js
+++ src/components/Statistics/UserSummary.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { fetchStatsService } from "../../utilities/statistics-service";
+import { findUsersByID } from "../../utilities/users-api";
 
 const UserSummaryStats = () => {
   const [statsLoading, setStatsLoading] = useState(false);
   const [stats, setStats] = useState(null);
   const [error, setError] = useState(null);
+  const [userDetails, setUserDetails] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,26 +25,73 @@ const UserSummaryStats = () => {
     fetchData();
   }, []);
 
-  const totalExpenses = stats ? stats.totalExpenses.totalExpenses : null;
+  useEffect(() => {
+    const fetchUserDetailsForStats = async () => {
+      if (
+        stats &&
+        stats.sharedExpenseOwed &&
+        stats.sharedExpenseOwed.usersThatOwes &&
+        stats.sharedExpenseOwed.usersThatOwes.length > 0
+      ) {
+        const userIds = stats.sharedExpenseOwed.usersThatOwes.map(
+          (item) => item.user
+        );
+        console.log("User IDs:", userIds); // Log user IDs
+
+        const userDetailsMap = {};
+        for (const userId of userIds) {
+          try {
+            const userDetails = await findUsersByID(userId);
+            console.log("User details for", userId, ":", userDetails); // Log user details
+
+            userDetailsMap[userId] = userDetails.user.username;
+          } catch (error) {
+            console.error("Error fetching user details:", error);
+            userDetailsMap[userId] = null;
+          }
+        }
+        console.log("User details map:", userDetailsMap); // Log user details map
+
+        setUserDetails(userDetailsMap);
+      }
+    };
+
+    fetchUserDetailsForStats();
+  }, [stats]);
+
+  const expensesCreated = stats ? stats.expensesCreated.expensesCreated : null;
+
+  const totalSharedExpenses = stats
+    ? stats.totalSharedExpenses.totalOwed
+    : null;
   const totalUnpaidSharedExpenses = stats
     ? stats.unpaidSharedExpenses.totalUnpaid
     : null;
-  const totalAmountToCollect = stats
-    ? stats.sharedExpenseStats.totalAmountIsOwed
+  const totalPaidSharedExpenses = stats
+    ? stats.paidSharedExpenses.totalPaid
     : null;
+  const totalAmountToCollect = stats
+    ? stats.sharedExpenseOwed && stats.sharedExpenseOwed.totalAmountIsOwed
+    : null;
+  const totalExpenses = stats ? stats.totalExpenses : null;
 
+  const UserOwesList = ({ usersThatOwes, userDetails }) => (
+    <div>
+      <ul>
+        {usersThatOwes.map((item, index) => (
+          <li key={index}>
+            {userDetails[item.user]} owed you {item.amountOwed}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
   return (
     <div className="stats-table">
       {statsLoading && <p>Loading...</p>}
       {stats && (
         <table>
           <tbody>
-            <tr>
-              <th>
-                Total Expenses<span>:</span>
-              </th>
-              <td>{totalExpenses}</td>
-            </tr>
             <tr>
               <th>
                 Unpaid Shared Expenses<span>:</span>
@@ -54,6 +103,12 @@ const UserSummaryStats = () => {
                 Total Amount To Collect<span>:</span>
               </th>
               <td>{totalAmountToCollect}</td>
+            </tr>
+            <tr>
+              <th>
+                Total Expenses<span>:</span>
+              </th>
+              <td>{totalExpenses}</td>
             </tr>
           </tbody>
         </table>
