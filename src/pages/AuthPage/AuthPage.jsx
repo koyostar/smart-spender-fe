8b644187b1c563f9faf147a9544e11b6fb85ec15
geewjh <geewjh@gmail.com>
2024-05-08 01:51:16 +0800

    style: style AuthPage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,20 +1,29 @@
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function AuthPage({ setUser }) {
   const location = useLocation();
 
   return (
-    <div className="bg-black h-[100vh] text-white">
-      <main className="container flex mx-auto h-[80vh] items-center justify-center">
+    <>
+      <span className="font-bebas whitespace-nowrap absolute top-4 left-4">
+        <Link to="/">
+          <span className="text-[#004F8F] text-4xl font-bold">
+            $mart $pender
+          </span>
+        </Link>
+      </span>
+
+      <main className="text-white p-4 container flex mx-auto min-h-screen items-center justify-center">
         {location.pathname === "/signup" ? (
           <SignUpForm setUser={setUser} />
         ) : location.pathname === "/login" ? (
           <LoginForm setUser={setUser} />
         ) : null}
       </main>
-    </div>
+    </>
   );
 }
 export default AuthPage;
